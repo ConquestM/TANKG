c3d3bd410812bd3260eeb136e1ad74d4224ad328
Faerghal1 <freid2@students.mags.school.nz>
2024-06-14 11:57:59 +1200

    Updated Boss attack rate

--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -1,6 +1,6 @@
 extends Area2D
 var speed = 500
-
+var despawn = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -8,4 +8,7 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	move_local_x(speed * delta) 
+	move_local_x(speed * delta)
+	despawn += 1
+	if despawn > 500:
+		queue_free()
