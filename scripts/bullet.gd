f358ef49ee49422993f5c4f2c5afc833f3a5b071
ConquestM <chirsxue123@gmail.com>
2024-06-14 12:02:43 +1200

    michael

--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -1,5 +1,6 @@
 extends Area2D
 var speed = 500
+var despawn = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -8,4 +9,7 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	despawn += 1
 	move_local_x(speed * delta) 
+	if despawn > 500:
+		queue_free()
