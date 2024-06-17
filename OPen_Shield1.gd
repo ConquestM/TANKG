5f59adef894c8cfae98b052107ff0a07ce5e308e
Faerghal1 <freid2@students.mags.school.nz>
2024-06-17 12:23:57 +1200

    Added Functionality to the Shield
    The shield now works but the bullet can phase through.

--- OPen_Shield1.gd
+++ OPen_Shield1.gd
@@ -1,6 +1,3 @@
 extends Area2D
 
 
-func _on_area_entered(area):
-	if area.has_meta("Attack")
-	
--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -10,5 +10,8 @@ func _ready():
 func _process(delta):
 	move_local_x(speed * delta)
 	despawn += 1
-	if despawn > 500:
+	if despawn > 300:
+		queue_free()
+func _on_area_entered(area):
+	if area.has_meta("Block"):
 		queue_free()
