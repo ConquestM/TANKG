fcd5f7c8321b5a1828e8e9aff92691f8d264b247
ConquestM <chirsxue123@gmail.com>
2024-06-24 09:12:52 +1200

    Diagonal movement now possible

--- scripts/player.gd
+++ scripts/player.gd
@@ -16,14 +16,35 @@ func _process(_delta):
 	var vert_direction = Input.get_axis("ui_up", "ui_down")
 	if direction:
 		velocity.x = direction * speed
+		if Input.is_action_pressed("ui_right"):
+			rotation_degrees = 0
+		if Input.is_action_pressed("ui_left"):
+			rotation_degrees = 180
 		$CollisionShape2D.scale.x = direction
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed)
 	if vert_direction:
 		velocity.y = vert_direction * speed
-		$CollisionShape2D.scale.x = direction
+		if Input.is_action_pressed("ui_up"):
+			rotation_degrees = 270
+		if Input.is_action_pressed("ui_down"):
+			rotation_degrees = 90
+		$CollisionShape2D.scale.x = vert_direction
 	else:
 		velocity.y = move_toward(velocity.y, 0, speed)
+	if direction and vert_direction:
+		if Input.is_action_pressed("ui_up") \
+		and Input.is_action_pressed("ui_right"):
+			rotation_degrees = 300
+		if Input.is_action_pressed("ui_up") \
+		and Input.is_action_pressed("ui_left"):
+			rotation_degrees = 225
+		if Input.is_action_pressed("ui_down") \
+		and Input.is_action_pressed("ui_right"):
+			rotation_degrees = 45
+		if Input.is_action_pressed("ui_down") \
+		and Input.is_action_pressed("ui_left"):
+			rotation_degrees = 135
 	move_and_slide()
 	global_position.x = clamp(global_position.x, 0, screen_size.x
 	 / get_parent().get_child(-1).zoom.x)
