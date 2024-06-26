682b7a792fad85a061eeb7fb65f7f9509ad3fa04
Faerghal1 <freid2@students.mags.school.nz>
2024-06-27 01:47:56 +1200

    implementing dash.

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,6 +4,14 @@ extends CharacterBody2D
 var screen_size
 @export var bullet_scene: PackedScene
 var can_shoot = true
+var dashing = false
+var dash_duration = 0.1
+var dash_timer = 0
+var can_dash = true
+var direction = 0
+var vert_direction = 0
+var saved_direction = 0
+var saved_vert_direction = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -11,22 +19,71 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
-	var direction = Input.get_axis("ui_left", "ui_right")
-	var vert_direction = Input.get_axis("ui_up", "ui_down")
+func _process(delta):
+	if not dashing:
+		direction = Input.get_axis("ui_left", "ui_right")
+		vert_direction = Input.get_axis("ui_up", "ui_down")
+	if direction > 0 or direction < 0 or vert_direction > 0 or vert_direction < 0:
+		saved_direction = direction
+		saved_vert_direction = vert_direction
 	if direction:
 		velocity.x = direction * speed
+		if Input.is_action_pressed("ui_right"):
+			rotation_degrees = 0
+		if Input.is_action_pressed("ui_left"):
+			rotation_degrees = 180
 		$CollisionShape2D.scale.x = direction
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed)
 	if vert_direction:
 		velocity.y = vert_direction * speed
-		$CollisionShape2D.scale.x = direction
+		if Input.is_action_pressed("ui_up"):
+			rotation_degrees = 270
+		if Input.is_action_pressed("ui_down"):
+			rotation_degrees = 90
+		$CollisionShape2D.scale.x = vert_direction
 	else:
 		velocity.y = move_toward(velocity.y, 0, speed)
+	if direction and vert_direction:
+		if Input.is_action_pressed("ui_up") \
+		and Input.is_action_pressed("ui_right"):
+			rotation_degrees = 300
+		if Input.is_action_pressed("ui_up") \
+		and Input.is_action_pressed("ui_left"):
+			rotation_degrees = 225
+		if Input.is_action_pressed("ui_down") \
+		and Input.is_action_pressed("ui_right"):
+			rotation_degrees = 45
+		if Input.is_action_pressed("ui_down") \
+		and Input.is_action_pressed("ui_left"):
+			rotation_degrees = 135
 	move_and_slide()
 	global_position.x = clamp(global_position.x, 0, screen_size.x
 	 / get_parent().get_child(-1).zoom.x)
 	global_position.y = clamp(global_position.y, 0, screen_size.y
 	 / get_parent().get_child(-1).zoom.y)
+	if Input.is_action_just_pressed("dash"):
+		if can_dash:
+			_dash()
+	if dashing:
+		dash_timer += delta
+		if dash_timer >= dash_duration:
+			dashing = false
+			speed = 100
+			dash_timer = 0
 
+func _dash():
+	if dashing: return
+	if Input.is_action_pressed("dash"):
+		dashing = true
+		if direction == 0 and vert_direction == 0:
+			speed = 500
+		else:
+			speed = 1000
+		direction = saved_direction
+		vert_direction = saved_vert_direction
+		can_dash = false
+		$Timer.start()
+
+func _on_timer_timeout():
+	can_dash = true
