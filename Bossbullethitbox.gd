443c8e32094ab8e5c6719e865eedb644544d46ea
Faerghal1 <freid2@students.mags.school.nz>
2024-07-25 12:08:41 +1200

    Added the deleting hitbox for the boss bullet

--- Bossbullethitbox.gd
+++ Bossbullethitbox.gd
@@ -3,9 +3,15 @@ extends Area2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	#get_child(0).start()
 	set_meta("Debt", 0)
+	$Delete.start()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	pass
+
+
+func _on_delete_timeout():
+	queue_free()
--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -10,6 +10,7 @@ var friendly = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	if not friendly:
+		set_meta("Delete", 0)
 		var hitbox = explosion_scene.instantiate()
 		hitbox.global_position = get_node("/root/Map/Player").global_position
 		hitbox.rotation = rotation+1.5
@@ -33,100 +34,10 @@ func _on_area_entered(area):
 		global.BossHP -= 1
 		queue_free()
 	if area.has_meta("Debt"):
-		print("det")
-	var megabulletb = megabullet2_scene	.instantiate()
-	add_sibling(megabulletb)
-	megabulletb.global_position = global_position
-	megabulletb.rotation = global_rotation
-	
-	var megabullet2b = megabullet2_scene.instantiate()
-	add_sibling(megabullet2b)
-	megabullet2b.global_position = global_position
-	megabullet2b.rotation = global_rotation+10
-	
-	var megabullet3b = megabullet2_scene.instantiate()
-	add_sibling(megabullet3b)
-	megabullet3b.global_position = global_position
-	megabullet3b.rotation = global_rotation-10
-	
-	var megabullet4b = megabullet2_scene.instantiate()
-	add_sibling(megabullet4b)
-	megabullet4b.global_position = global_position
-	megabullet4b.rotation = global_rotation-20
-	
-	var megabullet5b = megabullet2_scene.instantiate()
-	add_sibling(megabullet5b)
-	megabullet5b.global_position = global_position
-	megabullet5b.rotation = global_rotation+20
-	
-	var megabullet6b = megabullet2_scene.instantiate()
-	add_sibling(megabullet6b)
-	megabullet6b.global_position = global_position
-	megabullet6b.rotation = global_rotation-30
-	
-	var megabullet7b = megabullet2_scene.instantiate()
-	add_sibling(megabullet7b)
-	megabullet7b.global_position = global_position
-	megabullet7b.rotation = global_rotation+30
-	
-	var megabullet8b = megabullet2_scene.instantiate()
-	add_sibling(megabullet8b)
-	megabullet8b.global_position = global_position
-	megabullet8b.rotation = global_rotation-40
-	
-	var megabullet9b = megabullet2_scene.instantiate()
-	add_sibling(megabullet9b)
-	megabullet9b.global_position = global_position
-	megabullet9b.rotation = global_rotation+40
-	
-	var megabullet10b = megabullet2_scene.instantiate()
-	add_sibling(megabullet10b)
-	megabullet10b.global_position = global_position
-	megabullet10b.rotation = global_rotation-50
-	
-	var megabullet11b = megabullet2_scene.instantiate()
-	add_sibling(megabullet11b)
-	megabullet11b.global_position = global_position
-	megabullet11b.rotation = global_rotation+50
-	
-	var megabullet12b = megabullet2_scene.instantiate()
-	add_sibling(megabullet12b)
-	megabullet12b.global_position = global_position
-	megabullet12b.rotation = global_rotation-60
-	
-	var megabullet13b = megabullet2_scene.instantiate()
-	add_sibling(megabullet13b)
-	megabullet13b.global_position = global_position
-	megabullet13b.rotation = global_rotation+60
-	
-	var megabullet14b = megabullet2_scene.instantiate()
-	add_sibling(megabullet14b)
-	megabullet14b.global_position = global_position
-	megabullet14b.rotation = global_rotation-70
-	
-	var megabullet15b = megabullet2_scene.instantiate()
-	add_sibling(megabullet15b)
-	megabullet15b.global_position = global_position
-	megabullet15b.rotation = global_rotation+70
-	
-	var megabullet16b = megabullet2_scene.instantiate()
-	add_sibling(megabullet16b)
-	megabullet16b.global_position = global_position
-	megabullet16b.rotation = global_rotation-80
-	
-	var megabullet17b = megabullet2_scene.instantiate()
-	add_sibling(megabullet17b)
-	megabullet17b.global_position = global_position
-	megabullet17b.rotation = global_rotation+80
-	
-	var megabullet18b = megabullet2_scene.instantiate()
-	add_sibling(megabullet18b)
-	megabullet18b.global_position = global_position
-	megabullet18b.rotation = global_rotation-90
-	
-	var megabullet19b = megabullet2_scene.instantiate()
-	add_sibling(megabullet19b)
-	megabullet19b.global_position = global_position
-	megabullet19b.rotation = global_rotation+90
-
-	queue_free()
+		if not friendly:
+			for i in 20:
+				var megabulletb = megabullet2_scene	.instantiate()
+				add_sibling(megabulletb)
+				megabulletb.global_position = global_position
+				megabulletb.rotation_degrees = global_rotation_degrees + i*18
+				queue_free()
