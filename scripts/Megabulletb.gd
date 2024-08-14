b9d1e59538c974d55566f046e017cd895d44731e
Faerghal1 <freid2@students.mags.school.nz>
2024-08-15 11:08:30 +1200

    Added boos 2 attacks

--- scripts/Megabulletb.gd
+++ scripts/Megabulletb.gd
@@ -47,3 +47,7 @@ func _on_area_entered(area):
 
 func _on_die_timeout():
 	queue_free()
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	queue_free()
--- scripts/global.gd
+++ scripts/global.gd
@@ -1,7 +1,7 @@
 extends Node
 
 var HP = 15
-var BossHP = 10
+var BossHP = 50
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
