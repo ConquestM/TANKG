ca6648543a076290ae2020ab54d17f4aaba51c6c
Faerghal1 <freid2@students.mags.school.nz>
2024-10-14 19:49:08 +1300

    summerised boss 3 attacks/moves

--- scripts/boltbullet.gd
+++ scripts/boltbullet.gd
@@ -1,24 +1,43 @@
 extends Area2D
 @export var explosion_scene: PackedScene
+@export var megabullet2_scene: PackedScene
 @onready var global = get_node("/root/global")
-var direction = 0
+
 var speed = 50
 var despawn = 0
 var megabullet
-
 var friendly = false
- 
+
+# Called when the node enters the scene tree for the first time.
 func _ready():
-	$HomeTimer.start()
+	if friendly:
+		set_meta("player", 0)
+		
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	move_local_x(speed * delta)
 
-		move_local_x(speed * delta * 3)
-
+	if not friendly:
+		$Sprite2D.self_modulate -= Color(0,0.1,0.1,-0.1)
+	
 func _on_area_entered(area):
-	if area.has_meta("Player") and not friendly and not global.iframes:
-		global.HP -= 1
-		global.iframes = true
+	if area.has_meta("Spawn"):
 		queue_free()
-
+	if area.has_meta("Tile"):
+		queue_free()
+	if area.has_meta("Player") and not friendly:
+		global.HP -= 1
+		queue_free()
+	if area.has_meta("Boss") and friendly:
+		global.BossHP -= 1
+		queue_free()
+	if area.has_meta("Debt"):
+		if not friendly:
+			for i in 20:
+				var megabulletb = megabullet2_scene	.instantiate()
+				add_sibling(megabulletb)
+				megabulletb.global_position = global_position
+				megabulletb.rotation_degrees = global_rotation_degrees + i*18
+				queue_free()
 
--- scripts/global.gd
+++ scripts/global.gd
@@ -6,6 +6,7 @@ var iframes = false
 var timer
 var level = 0
 var spirits = 0
+var zombies = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
