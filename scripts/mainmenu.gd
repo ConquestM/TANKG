2023567beb130aaabc0fdb82eba7b3871fc34cef
ConquestM <chirsxue123@gmail.com>
2024-10-15 10:59:24 +1300

    iframes fix

--- scripts/mainmenu.gd
+++ scripts/mainmenu.gd
@@ -24,3 +24,7 @@ func _on_level_2_pressed():
 
 func _on_level_3_pressed():
 	get_tree().change_scene_to_file("res://Level3.tscn")
+
+
+func _on_settings_pressed():
+	get_tree().change_scene_to_file("res://")
